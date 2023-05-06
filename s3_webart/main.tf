1bec9e304f6860e6a3a6dbcc38b90b02169a04a3
ArtemShavlak <129053080+ArtemShavlak@users.noreply.github.com>
2023-05-06 22:13:00 +0300

    Update main.tf

--- s3_webart/main.tf
+++ s3_webart/main.tf
@@ -1,4 +1,10 @@
 terraform {
+	backend "s3" { 
+		Bucket = "lab1-artem-bucket" 
+		key = "terraform.tfstate" 
+		region = "us-east-1" 
+		dynamodb_table = "lab1_table" 
+	} 
 	required_providers {
 		aws = {
 		source = "hashicorp/aws"
